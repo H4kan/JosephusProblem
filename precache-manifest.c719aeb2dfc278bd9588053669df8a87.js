4fdc4503b5714d0b66e48a4d6776bd90c8ca0127
Szymon Sieradzki <szymon.sieradzki184gmail.com>
2020-05-22 19:12:11 +0200

    Updates

---
+++ precache-manifest.c719aeb2dfc278bd9588053669df8a87.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "198a9d14ed77ab79af14",
+    "url": "/JosephusProblem/static/js/runtime~main.2ec9a710.js"
+  },
+  {
+    "revision": "6e51c2d0ff82ffbe8b30",
+    "url": "/JosephusProblem/static/js/main.da7826e8.chunk.js"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/JosephusProblem/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "6e51c2d0ff82ffbe8b30",
+    "url": "/JosephusProblem/static/css/main.aff3e798.chunk.css"
+  },
+  {
+    "revision": "84d5c00576896d79ede52415bd1c989a",
+    "url": "/JosephusProblem/index.html"
+  }
+];
